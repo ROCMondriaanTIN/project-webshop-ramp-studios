55928aeff8e16a01fd346e5170e0b026de936743
Thong <thop01@rocmondriaan.nl>
2020-02-11 13:21:33 +0100

    Added api call

--- src/frontend/js/AdminController.js
+++ src/frontend/js/AdminController.js
@@ -12,18 +12,7 @@ class Admin{
     
     async init(){
         document.getElementById("header").append(this.loginComponent.getView());
-        
-        let form = document.getElementById("addProduct");
-        form.addEventListener("submit", (evt) => {
-            evt.preventDefault();
-            let form = document.getElementById("addProduct");
-            let formData = new FormData(form);
-            for(let p of formData) {
-                console.log(p);
-            }
-            api.addProduct(formData);
-        });
-        
+                
         // Check with the api if there is already al auth token.
         if(api.isAuthenticated) {
             // Get the logged in user
@@ -40,5 +29,62 @@ class Admin{
                 window.location = '/';
             }
         }
+
+        // Add a product
+        let formAddProduct = document.getElementById("addProduct");
+        formAddProduct.addEventListener("submit", async (evt) => {
+            evt.preventDefault();
+            let form = document.getElementById("addProduct");
+            let formData = new FormData(form);
+            for(let p of formData) {
+                console.log(p);
+            }
+            try {
+                const data = await api.addProduct(formData);
+                console.log(data);
+                window.location = '/';
+            }
+            catch(err) {
+                console.error(err);
+                formAddProduct.setAttribute("class", "error");
+            }
+        });
+        
+        // Restock a product
+        let formRestock = document.getElementById("restockProduct");
+        formRestock.addEventListener("submit", async (evt) => {
+            evt.preventDefault();
+            console.log("Restocking");
+            let productId = document.getElementById("restockProductId").value;
+            let quantity = document.getElementById("restockQuantity").value;
+            api.restockProduct(productId, quantity);
+            try {
+                const data = await api.restockProduct(productId, quantity);
+                console.log(data);
+                window.location = '/';
+            }
+            catch(err) {
+                console.error(err);
+                formRestock.setAttribute("class", "error");
+            }
+        });
+        
+        // Detele a product
+        let formDelete = document.getElementById("deleteProduct");
+        console.log(formDelete);
+        formDelete.addEventListener("submit", async (evt) => {
+            evt.preventDefault();
+            console.log("Deleting");
+            let productId = document.getElementById("deleteProductId").value;
+            try {
+                const data = await api.deleteProduct(productId);
+                console.log(data);
+                window.location = '/';
+            }
+            catch(err) {
+                console.error(err);
+                formDelete.setAttribute("class", "error");
+            }
+        });
     }
 }
